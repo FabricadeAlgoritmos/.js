287fc26dd4e7d1e3cead76342297a6cb291da7c1
fabricadealgoritmos <fabricadealgoritmos@gmail.com>
2017-08-13 17:28:56 -0300

    bubblesort m1

---
+++ BubbleSort/v1/main.js
@@ -0,0 +1,18 @@
+var vetor = [5, 4, 3, 2, 1];
+
+var max = 4;
+var troca = 1;
+
+while(troca != 0){
+    troca = 0;
+    for(var i = 0; i <= max - 1; i++){
+        if(vetor[i] > vetor[i + 1]){
+            var aux = vetor[i];
+            vetor[i] = vetor[i + 1];
+            vetor[i + 1] = aux;
+            troca = 1;
+        }
+    }
+}
+
+console.log(vetor);
